77122af345c27adcb8d5cfabf0a176ccd9117dc4
guangliwoodson <guangliwoodson@outlook.com>
2022-07-17 23:26:21 +0800

    test_1

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,64 @@
 <template>
-	<div class="container">
-		<div class="imglist">
-			<h3>图片列表区</h3>
-			<img src="./assets/01.jpg" alt="" @mouseenter="enterFn(1)" @click="clickFn(1, $event)">
-			<img src="./assets/02.jpg" alt="" @mouseenter="enterFn(2)" @click="clickFn(2, $event)">
-			<img src="./assets/03.jpg" alt="" @mouseenter="enterFn(3)" @click="clickFn(3, $event)">
-		</div>
-		<div class="show">
-			<h3>图片预览区</h3>
-			<img :src="imgUrl" alt="" style="width:600px;height:auto;">
-		</div>
-		<div>
-		</div>
-	</div>
+  <div id="app">
+    <div id="app">
+    <!-- 语法1:
+      v-for="(值变量名, 索引变量名) in 目标结构"
+      口诀: 想要谁循环就放到谁身上 -->
+      <p>学生姓名</p>
+      <ul>
+        <li v-for="(item, index) in arr" :key="item">
+          {{ index }} - {{ item }}
+        </li>
+      </ul>
+
+      <p>学生详细信息</p>
+      <ul>
+        <li v-for="obj in stuArr" :key="obj.id">
+          <span>{{ obj.name }}</span>
+          <span>{{ obj.sex }}</span>
+          <span>{{ obj.hobby }}</span>
+        </li>
+      </ul>
+
+      <!-- v-for遍历对象(了解) -->
+      <p>老师信息</p>
+      <div v-for="(value, key) in tObj" :key="value">
+        {{ key }} -- {{ value }}
+      </div>
+
+      <!-- v-for遍历整数(了解) - 从1开始 -->
+      <p>序号</p>
+      <div v-for="i in count" :key="i">{{ i }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import img_01 from '@/assets/01.jpg';
-import img_02 from '@/assets/02.jpg';
-import img_03 from '@/assets/03.jpg';
 export default {
-	data() {
-		return {
-			imgUrl: img_01
-		};
-	},
-	methods: {
-		enterFn(val) {
-			this.imgUrl = { 1: img_01, 2: img_02, 3: img_03 }[val]
-		},
-		clickFn(num, e) {
-			alert(`第${num}张图${e.target.src}`);
-		}
-	}
-}
+  data() {
+    return {
+      arr: ["小明", "小欢欢", "大黄"],
+      stuArr: [
+        {
+          id: 1001,
+          name: "孙悟空",
+          sex: "男",
+          hobby: "吃桃子",
+        },
+        {
+          id: 1002,
+          name: "猪八戒",
+          sex: "男",
+          hobby: "背媳妇",
+        },
+      ],
+      tObj: {
+        name: "小黑",
+        age: 18,
+        class: "1期",
+      },
+      count: 10,
+    };
+  },
+};
 </script>
-
-<style scoped>
-.container {
-	display: flex;
-}
-
-.imglist {
-	border: 2px solid red;
-}
-
-.imglist img {
-	width: 200px;
-	height: auto;
-}
-
-.show {
-	border: 2px solid red;
-	margin-right: 20px;
-}
-</style>
